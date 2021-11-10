04bb83a0ce8038df692e5cb58a999b76cf32077e
Lanterman <splen1011@mail.ru>
2021-11-10 17:21:17 +0300

    1 Ten

---
+++ config/urls.py
@@ -0,0 +1,24 @@
+import debug_toolbar
+from django.conf.urls import url
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from . import settings
+from .yasg import urlpatterns as doc_urls
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
+    path('auth/', include('djoser.urls.jwt')),
+    url('', include('social_django.urls', namespace='social')),
+    path('__debug__/', include(debug_toolbar.urls)),
+]
+
+urlpatterns += doc_urls
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
